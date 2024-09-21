62a8d495450841e6198da008ffa5baceb476375b
HamzaTahir22 <Hamzatahirn9005@gmail.com>
2024-09-22 00:34:50 +0500

    Added All Nurse Related Routes and Controllers

--- src/models/Nurse.js
+++ src/models/Nurse.js
@@ -9,17 +9,22 @@ const NurseSchema = new mongoose.Schema({
   assignedPatients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient' }], // Assigned patients
   role: { type: String, default: 'nurse', immutable: true },  // Make role immutable means it can not be changed
   lastPasswordChange: { type: Date, default: Date.now },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+}, {
+  timestamps: true, // Automatically handles createdAt and updatedAt
 });
 
-// Hash the password before saving
 NurseSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+  this.lastPasswordChange = Date.now();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// Define a method to check if the entered password matches the stored one
+NurseSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 const Nurse = mongoose.model('Nurse', NurseSchema);
 
