be5682af8f02f6ee44989533a37794b43ab6bcc0
usama2017 <edu.usamazahid@gmail.com>
2024-12-03 01:03:13 +0500

    Added Swagger to my APIs

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,8 +2,49 @@ const Patient = require('../models/Patient');
 const HealthRecord = require('../models/HealthRecord');
 const Task = require('../models/Task');
 const CarePlan = require('../models/CarePlan');
+const SupportTicket = require('../models/SupportTicket');
 
-// Fetch patient overview
+/**
+ * @swagger
+ * /api/v1/admin/patient-overview/{patientId}:
+ *   get:
+ *     summary: Fetch detailed patient overview
+ *     tags: [Admin]
+ *     parameters:
+ *       - in: path
+ *         name: patientId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the patient
+ *     responses:
+ *       200:
+ *         description: Detailed patient overview
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 patient:
+ *                   $ref: '#/components/schemas/Patient'
+ *                 healthRecords:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/HealthRecord'
+ *                 carePlan:
+ *                   $ref: '#/components/schemas/CarePlan'
+ *                 tasks:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Task'
+ *                 taskCompletionRate:
+ *                   type: number
+ *                   description: Percentage of completed tasks
+ *       404:
+ *         description: Patient not found
+ *       500:
+ *         description: Error fetching patient overview
+ */
 exports.getPatientOverview = async (req, res) => {
   try {
     const { patientId } = req.params;
@@ -38,7 +79,35 @@ exports.getPatientOverview = async (req, res) => {
   }
 };
 
-// Create a support ticket
+/**
+ * @swagger
+ * /api/v1/admin/support-ticket:
+ *   post:
+ *     summary: Create a support ticket
+ *     tags: [Admin]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - subject
+ *               - description
+ *             properties:
+ *               subject:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               status:
+ *                 type: string
+ *                 default: open
+ *     responses:
+ *       201:
+ *         description: Support ticket created successfully
+ *       500:
+ *         description: Error creating support ticket
+ */
 exports.createSupportTicket = async (req, res) => {
   try {
     const { subject, description, status } = req.body;
@@ -57,7 +126,37 @@ exports.createSupportTicket = async (req, res) => {
   }
 };
 
-// Fetch all support tickets
+/**
+ * @swagger
+ * /api/v1/admin/support-tickets:
+ *   get:
+ *     summary: Fetch all support tickets
+ *     tags: [Admin]
+ *     parameters:
+ *       - in: query
+ *         name: status
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: Filter tickets by status (e.g., open, closed)
+ *       - in: query
+ *         name: userId
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: Filter tickets by user ID
+ *     responses:
+ *       200:
+ *         description: List of support tickets
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SupportTicket'
+ *       500:
+ *         description: Error fetching support tickets
+ */
 exports.getSupportTickets = async (req, res) => {
   try {
     const { status, userId } = req.query;
@@ -72,5 +171,3 @@ exports.getSupportTickets = async (req, res) => {
     res.status(500).json({ message: 'Error fetching support tickets', details: error.message });
   }
 };
-
-
