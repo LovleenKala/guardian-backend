5688b1fdc5760e673c33fa0706c5b2853499afd5
Deepak <deepak4881.be22@chitkara.edu.in>
2025-09-11 19:32:59 +1000

    Giving add patients permissions only to caretaker

--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -5,7 +5,14 @@ const verifyToken = require('../middleware/verifyToken');
 const verifyRole = require('../middleware/verifyRole');
 const upload = require('../middleware/multer');
 
-router.post('/add', verifyToken, upload.single('photo'), patientController.addPatient);
+router.post(
+    '/add',
+    verifyToken,
+    verifyRole(['caretaker']),
+    upload.single('photo'),
+    patientController.addPatient
+  );
+router.delete('/:patientId', verifyToken, patientController.deletePatient);
 router.post('/assign-nurse', verifyToken, verifyRole(['caretaker']), patientController.assignNurseToPatient);
 router.get('/assigned-patients', verifyToken, patientController.getAssignedPatients);
 router.get('/', verifyToken, patientController.getPatientDetails);
